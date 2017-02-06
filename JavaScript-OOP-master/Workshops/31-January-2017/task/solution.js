f64567a6fd32584329087f85d462f40db7c97627
Doroteya Nikolova <doroteanikolova93@gmail.com>
2017-02-06 18:14:12 +0200

    solve workshop problem..

--- JavaScript-OOP-master/Workshops/31-January-2017/task/solution.js
+++ JavaScript-OOP-master/Workshops/31-January-2017/task/solution.js
@@ -1,14 +1,45 @@
-function solve() {
-	function getProduct(productType, name, price) {
-	}
+'use strict';
 
-	function getShoppingCart() {
-	}
+// function solve() {
+function getProduct(productType, name, price) {
+    this.productType = productType;
+    this.name = name;
+    this.price = price;
 
-	return {
-		getProduct: getProduct,
-		getShoppingCart: getShoppingCart
-	};
+    return this;
 }
+
+function getShoppingCart() {
+    let products = [];
+    let cart = {
+        add: function(product) {
+            let self = this,
+                addProduct = {};
+            addProduct = {
+                productType: product[0],
+                name: product[1],
+                price: product[2],
+            }
+
+            products.push(addProduct);
+        },
+        remove: function(product) {},
+        showCost: function() {
+            return this.price;
+        },
+        showProductType: function() {},
+        getInfo: function() {},
+
+    }
+
+    return products;
+
+}
+
+//     return {
+//         getProduct: getProduct,
+//         getShoppingCart: getShoppingCart
+//     };
+// }
 
 module.exports = solve();
